2992ae945c373d13191a0d8a2bfad83fc5ee304b
Dhanush Alagandula <dhanushalagandula@gmail.com>
2026-01-16 17:15:05 +0530

    Added cors

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
-
+const cors = require('cors');
 const connectDB = require('./config/db');
 connectDB();
+
+//use cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+};
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
