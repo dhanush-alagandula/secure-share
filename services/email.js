2d2c8bfecd2bbaed814aa6c6e2b48dd9187964fe
Dhanush Alagandula <dhanushalagandula@gmail.com>
2026-01-23 12:05:04 +0530

    Fixed Deployment issue

--- services/email.js
+++ services/email.js
@@ -4,11 +4,14 @@ async function sendEmail({ from, to, subject, text, html }) {
     let transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: Number(process.env.EMAIL_PORT),
-        secure: true, // true for 465, false for other ports
+        secure: false, // false for 587, true for 465
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
-        }
+        },
+        connectionTimeout: 10000, // 10 seconds
+        greetingTimeout: 10000, // 10 seconds
+        socketTimeout: 10000 // 10 seconds
     });
 
     let info = await transporter.sendMail({
