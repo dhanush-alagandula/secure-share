d5d087e39dc19c67595c4f24e1289d7e148ce9ae
Dhanush Alagandula <dhanushalagandula@gmail.com>
2026-01-23 12:14:16 +0530

    Fixed Deployment issue

--- services/email.js
+++ services/email.js
@@ -5,6 +5,7 @@ async function sendEmail({ from, to, subject, text, html }) {
         host: process.env.EMAIL_HOST,
         port: Number(process.env.EMAIL_PORT),
         secure: false, // false for 587, true for 465
+        requireTLS : true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
