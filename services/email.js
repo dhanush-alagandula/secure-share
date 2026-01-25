7c3e19fc8b1c456eb46789731817805a33042c99
Dhanush Alagandula <dhanushalagandula@gmail.com>
2026-01-25 16:08:28 +0530

    Fixed SMTP issue

--- services/email.js
+++ services/email.js
@@ -4,8 +4,7 @@ async function sendEmail({ from, to, subject, text, html }) {
     let transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: Number(process.env.EMAIL_PORT),
-        secure: false, // false for 587, true for 465
-        requireTLS : true,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
